163d675378786c78a4c4a5df99de8faaa0cd9b93
LukasAnell <lukasmanell@gmail.com>
2026-05-23 16:40:31 -0700

    Removed planning comments

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -62,18 +62,6 @@ public class FileHandler {
             );
         }
 
-        // serve a file from rootDirectory based on the path specified in request
-
-        // find full file path
-        // if the path is just /, serve index.html from the rootDirectory
-
-        // if the file exists and is readable, return a response saying "200 OK"
-        // include 2 headers: Content-Type: MIME type, Content-Length: file size in bytes
-        //  file contents as the body of the response
-
-        // if the file doesn't exist, return a 404 Not Found response
-        // body is "404 Not Found"
-
         return new HttpResponse(status, headers, body.toString());
     }
 }
