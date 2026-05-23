2deef9fd356930f7a8e6f2381fe076441be4493a
LukasAnell <lukasmanell@gmail.com>
2026-05-23 13:36:51 -0700

    Add enum for possible HTTP statuses, w/ codes and reasons

---
+++ src/main/java/HttpStatus.java
@@ -0,0 +1,24 @@
+public enum HttpStatus {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String reason;
+
+    HttpStatus(int code, String reason) {
+        this.code = code;
+        this.reason = reason;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+}
