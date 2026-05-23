057bcc233ffd9189f6f89a5d7edd46393efca056
LukasAnell <lukasmanell@gmail.com>
2026-05-23 15:49:14 -0700

    Implemented HttpResponseBuilder methods

---
+++ src/main/java/HttpResponseBuilder.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpResponseBuilder {
+
+    private HttpStatus status;
+    private Map<String, String> headers;
+    private String body;
+
+    public HttpResponseBuilder() {
+        this.status = HttpStatus.OK;
+        this.headers = new HashMap<>();
+        this.body = "";
+    }
+
+    public HttpResponseBuilder setStatus(HttpStatus status) {
+        this.status = status;
+
+        return this;
+    }
+
+    public HttpResponseBuilder addHeader(String name, String value) {
+        headers.put(name, value);
+
+        return this;
+    }
+
+    public HttpResponseBuilder setBody(String body) {
+        this.body = body;
+
+        return this;
+    }
+
+    public HttpResponse build() {
+        return new HttpResponse(status, headers, body);
+    }
+}
