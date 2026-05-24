512b5416bdb111bc5560bfa28d68dd173a17b2a8
LukasAnell <lukasmanell@gmail.com>
2026-05-23 17:58:33 -0700

    Implemented dispatch, working on implementing hasRoute()

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -21,14 +21,45 @@ public class Router {
     }
 
     public HttpResponse dispatch(HttpRequest request) {
-        //
+        // get requested path
+        String path = request.getPath();
 
-        return null;
+        // get HttpMethod
+        HttpMethod method = request.getMethod();
+
+        if (!routingTable.containsKey(path)) {
+            return new HttpResponseBuilder()
+                .setStatus(HttpStatus.NOT_FOUND)
+                .addHeader("Content-Type", "text/plain")
+                .setBody("404 Not Found")
+                .build();
+        }
+
+        if (!routingTable.get(path).containsKey(method)) {
+            return new HttpResponseBuilder()
+                .setStatus(HttpStatus.METHOD_NOT_ALLOWED)
+                .addHeader("Content-Type", "text/plain")
+                .setBody("405 Method Not Allowed")
+                .build();
+        }
+
+        // create lambda Handler
+        // calls Handler.handle() on a given HttpRequest
+        Handler handler = r -> routingTable.get(path).get(method).handle(r);
+
+        // return result of handled request
+        return handler.handle(request);
     }
 
     public boolean hasRoute(HttpMethod method, String path) {
-        //
+        if (!routingTable.containsKey(path)) {
+            return false;
+        }
 
-        return false;
+        if (!routingTable.containsKey(method)) {
+            return false;
+        }
+
+        return true;
     }
 }
